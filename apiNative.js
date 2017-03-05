eb56dde3e560eb7ba7c43ea40e03e919a8ebd85c
Mehdi <mehdi@mehdi-aberkane.fr>
2017-03-05 12:54:35 +0100

    get post data

--- apiNative.js
+++ apiNative.js
@@ -29,17 +29,22 @@ exports.server = http.createServer((req, res) => {
                 var post = qs.parse(body);
                 console.log(post);
             });
-        }
-        if (err) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(getErrorPage());
+
+            res.writeHead(302, {
+                'Location': 'index'
+            });
+            res.end();
         } else {
-            //let query = url.parse(req.url, true).query;
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
+            if (err) {
+                res.statusCode = 404;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(getErrorPage());
+            } else {
+                //let query = url.parse(req.url, true).query;
+                res.statusCode = 200;
+                res.setHeader('Content-Type', 'text/html');
+                res.end(data);
+            }
         }
     })
-
 });
