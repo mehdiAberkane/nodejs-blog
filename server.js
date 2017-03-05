c48d03f9cd1cc861bf742199a8bc2b4e54a82035
Mehdi <mehdi@mehdi-aberkane.fr>
2017-03-05 12:47:42 +0100

    add apiNative for simple example of routing

--- server.js
+++ server.js
@@ -1,36 +1,9 @@
-//import lib
-const http = require('http');
-const util = require('util');
-const fs = require('fs');
-const url = require('url');
-
 //config global
 const hostname = '127.0.0.1';
 const port = 3000;
 
-function getErrorPage()
-{
-    var data = fs.readFileSync('404.html');
+var apiNative = require("./apiNative");
 
-    return data;
-}
-
-const server = http.createServer((req, res) => {
-    fs.readFile('index.html', (err, data) => {
-        if (err) {
-            res.statusCode = 404;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(getErrorPage());
-        } else {
-            let query = url.parse(req.url, true).query;
-            console.log(query.name);
-            res.statusCode = 200;
-            res.setHeader('Content-Type', 'text/html');
-            res.end(data);
-        }
-    })
-});
-
-server.listen(port, hostname, () => {
+apiNative.server.listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
