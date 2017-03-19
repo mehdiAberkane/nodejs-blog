9e9702796f7c6ff26aa68413c90557b99f9957aa
Mehdi <mehdi@mehdi-aberkane.fr>
2017-03-19 20:04:13 +0100

    some change and test

--- app.js
+++ app.js
@@ -1,18 +1,41 @@
-var express = require('express');
-var app = express();
-var compression = require('compression');
-app.listen(3000);
+const express = require('express');
+const compression = require('compression');
+const favicon = require('serve-favicon');
+const mongoose = require('mongoose');
+const path = require('path');
+const bodyParser = require('body-parser');
 
-//middleware compression ex: gzip
+const app = express();
+app.listen(3000);
+app.use(bodyParser.json()); // support json encoded bodies
+app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
+
+mongoose.connect('mongodb://localhost/blog');
+//var Cat = mongoose.model('Cat', { name: String });
+//
+//var kitty = new Cat({ name: 'Zildjian' });
+//kitty.save(function (err) {
+//    if (err) {
+//        console.log(err);
+//    } else {
+//        console.log('meow');
+//    }
+//})
+
+//middleware compression/favicon
 app.use(compression());
+app.use(favicon(path.join(__dirname, 'public', 'img/fav.ico'), 0));
 
 //defined jade as view engine by default
 app.set('view engine', 'jade');
 app.set('views', './views');
+app.use(express.static('public'));
 
 app.get('/', function(req, res) {
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Vous êtes à l\'accueil');
+        var articles = {"title": "mon titre", "slug": "mon-titre", "content": "mon article"};
+
+        res.setHeader('Content-Type', 'text/html');
+        res.render('index', { articles });
     })
     .get('/contact', function(req, res) {
         res.setHeader('Content-Type', 'text/plain');
@@ -21,6 +44,13 @@ app.get('/', function(req, res) {
     .get('/articles/:article_name/', function(req, res) {
         res.setHeader('Content-Type', 'text/html');
         res.render('index', { title: req.params.article_name, message: 'Mon super article!'});
+    })
+    .post('/', (req, res) => {
+        var title = req.body.title;
+        var content = req.body.content;
+        if (title === undefined || content === undefined) {
+            res.redirect('/');
+        }
     })
 ;
 
--- database.js
+++
@@ -1,8 +0,0 @@
-const MongoClient = require('mongodb').MongoClient;
-
-// Connect to the db
-MongoClient.connect("mongodb://localhost:27017/blog", function(err, db) {
-    if(!err) { console.log("We are connected");  }
-
-    db.createCollection('test', function(err, collection) {});
-});
