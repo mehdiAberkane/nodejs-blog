8ce6545ec461274dc8ff38a95aed4cfa04b5f96a
Mehdi <mehdi@mehdi-aberkane.fr>
2017-03-21 00:08:58 +0100

    add chat

--- app.js
+++ app.js
@@ -3,9 +3,12 @@ const compression = require('compression');
 const favicon = require('serve-favicon');
 const path = require('path');
 const bodyParser = require('body-parser');
+
 const database = require('./database');
 const app = express();
 app.listen(3000);
+const server = require('http').createServer(app);
+const socketIo = require('socket.io').listen(server);
 
 database.mongoose.connect('mongodb://localhost/blog');
 
@@ -43,6 +46,12 @@ app.get('/', function(req, res) {
             res.render('article', {article: article[0] });
         });
     })
+    .get('/chat', function(req, res) {
+        res.setHeader('Content-Type', 'text/html');
+
+
+        res.render('chat', { });
+    })
     .post('/', function(req, res) {
         if (req.body.title.length == 0 || req.body.content.length == 0) {
             //gestion error
@@ -55,6 +64,23 @@ app.get('/', function(req, res) {
         res.redirect('/');
     })
 ;
+
+socketIo.sockets.on('connection', function (socket, pseudo) {
+    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
+    socket.on('nouveau_client', function(pseudo) {
+        pseudo = ent.encode(pseudo);
+        socket.pseudo = pseudo;
+        socket.broadcast.emit('nouveau_client', pseudo);
+    });
+
+    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
+    socket.on('message', function (message) {
+        message = ent.encode(message);
+        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
+    });
+});
+
+server.listen(8080);
 
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
---
+++ public/js/main.js
@@ -0,0 +1,89 @@
+(function () {
+    var Message;
+    Message = function (arg) {
+        this.text = arg.text, this.message_side = arg.message_side;
+        this.draw = function (_this) {
+            return function () {
+                var $message;
+                $message = $($('.message_template').clone().html());
+                $message.addClass(_this.message_side).find('.text').html(_this.text);
+                $('.messages').append($message);
+                return setTimeout(function () {
+                    return $message.addClass('appeared');
+                }, 0);
+            };
+        }(this);
+        return this;
+    };
+    $(function () {
+        var getMessageText, message_side, sendMessage;
+        message_side = 'right';
+        getMessageText = function () {
+            var $message_input;
+            $message_input = $('.message_input');
+            return $message_input.val();
+        };
+        sendMessage = function (text) {
+            var $messages, message;
+            if (text.trim() === '') {
+                return;
+            }
+            $('.message_input').val('');
+            $messages = $('.messages');
+            message_side = message_side === 'left' ? 'right' : 'left';
+            message = new Message({
+                text: text,
+                message_side: message_side
+            });
+            message.draw();
+            return $messages.animate({ scrollTop: $messages.prop('scrollHeight') }, 300);
+        };
+        $('.send_message').click(function (e) {
+            return sendMessage(getMessageText());
+        });
+        $('.message_input').keyup(function (e) {
+            if (e.which === 13) {
+                return sendMessage(getMessageText());
+            }
+        });
+        sendMessage('Hello Philip! :)');
+        setTimeout(function () {
+            return sendMessage('Hi Sandy! How are you?');
+        }, 1000);
+        return setTimeout(function () {
+            return sendMessage('I\'m fine, thank you!');
+        }, 2000);
+    });
+}.call(this));
+
+// Connexion à socket.io
+var socket = io.connect('http://localhost:3000');
+
+// On demande le pseudo, on l'envoie au serveur et on l'affiche dans le titre
+var pseudo = prompt('Quel est votre pseudo ?');
+socket.emit('nouveau_client', pseudo);
+document.title = pseudo + ' - ' + document.title;
+
+// Quand on reçoit un message, on l'insère dans la page
+socket.on('message', function(data) {
+    insereMessage(data.pseudo, data.message)
+})
+
+// Quand un nouveau client se connecte, on affiche l'information
+socket.on('nouveau_client', function(pseudo) {
+    $('#zone_chat').prepend('<p><em>' + pseudo + ' a rejoint le Chat !</em></p>');
+})
+
+// Lorsqu'on envoie le formulaire, on transmet le message et on l'affiche sur la page
+$('#formulaire_chat').submit(function () {
+    var message = $('#message').val();
+    socket.emit('message', message); // Transmet le message aux autres
+    insereMessage(pseudo, message); // Affiche le message aussi sur notre page
+    $('#message').val('').focus(); // Vide la zone de Chat et remet le focus dessus
+    return false; // Permet de bloquer l'envoi "classique" du formulaire
+});
+
+// Ajoute un message dans la page
+function insereMessage(pseudo, message) {
+    $('#zone_chat').prepend('<p><strong>' + pseudo + '</strong> ' + message + '</p>');
+}
