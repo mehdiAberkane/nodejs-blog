99823e6ee6ed16e96391c211ab5fdcb5cba3218f
Mehdi <mehdi@mehdi-aberkane.fr>
2017-03-24 13:56:38 +0100

    update chat socket.io

--- app.js
+++ app.js
@@ -85,23 +85,6 @@ app.get('/', function(req, res) {
     })
 ;
 
-socketIo.sockets.on('connection', function (socket, pseudo) {
-    // Dès qu'on nous donne un pseudo, on le stocke en variable de session et on informe les autres personnes
-    socket.on('nouveau_client', function(pseudo) {
-        pseudo = ent.encode(pseudo);
-        socket.pseudo = pseudo;
-        socket.broadcast.emit('nouveau_client', pseudo);
-    });
-
-    // Dès qu'on reçoit un message, on récupère le pseudo de son auteur et on le transmet aux autres personnes
-    socket.on('message', function (message) {
-        message = ent.encode(message);
-        socket.broadcast.emit('message', {pseudo: socket.pseudo, message: message});
-    });
-});
-
-server.listen(8080);
-
 app.use(function(req, res, next){
     res.setHeader('Content-Type', 'text/plain');
     res.send(404, 'Lost !');
