b1a5a3281ceb3c566fd5dcfa880f95b291791a64
Mehdi <mehdi@mehdi-aberkane.fr>
2017-06-05 21:46:22 +0200

    add default example

---
+++ example/default.js
@@ -0,0 +1,15 @@
+const http = require('http');
+
+//config global
+const hostname = '127.0.0.1';
+const port = 3000;
+
+const server = http.createServer((req, res) => {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/html');
+    res.end('salut');
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
