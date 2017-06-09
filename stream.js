9ee2f965307a9deeded63acb1b97e3b231455cca
Mehdi <mehdi@mehdi-aberkane.fr>
2017-06-09 12:40:38 +0200

    opti code

--- stream.js
+++ stream.js
@@ -1,14 +1,11 @@
 var fs = require('fs');
 
-
-
 //with no stream
 fs.readFile('monfichier.txt', function(err, data){
 	fs.writeFile('copie_monfichier.txt', data, function(){
 		console.log('ok');
 	})
 });
-
 
 //with stream
 
